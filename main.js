4688e5d95b47e0f0fe8b7cdae547f1590aa0ef7a
Uvais A <138773333+uvais2021@users.noreply.github.com>
2023-11-29 22:17:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+
+const form = document.getElementById('form');
+const btn = document.getElementById('btn');
+const nameu = document.getElementById('name');
+const phone = document.getElementById('phone');
+const getphone = document.getElementById('getphone');
+var a = 0;
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    checkInputs();
+    if (a == 2) {
+        display();
+    } else {
+        a = 0;
+    }
+});
+function checkInputs() {
+
+    const uname = nameu.value.trim();
+    const uphone = phone.value.trim();
+
+
+
+    if (uname === '') {
+        setErrorFor(nameu, 'Name should be filled')
+    }
+    else {
+        setSuccessFor(nameu);
+    }
+
+    if (uphone === '') {
+        setErrorFor(phone, 'phone number should be filled')
+    }
+    else {
+        setSuccessFor(phone);
+    }
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+    formControl.className = 'error input'
+}
+function setSuccessFor(input) {
+    a += 1
+    const formControl = input.parentElement;
+    formControl.className = 'success input'
+}
+function display() {
+    document.write(`<center>`);
+    document.write(`<h2>Your Inputs</h2>` + `<br>`);
+    document.write(`Name: ${nameu.value}` + `<br>`);
+
+    document.write(`Number: +${getphone.value} ${phone.value}` + `<br>`);
+
+
+}
+console.log('Running');
